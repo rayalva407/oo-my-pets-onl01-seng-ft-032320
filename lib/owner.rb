741785cdfc1f2ce9f12cf1acf4d4f335a21edc48
Ray Alva <rayalva407@gmail.com>
2020-04-08 16:57:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species, :cats
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    cat.all.collect do {|cat| cat.owner == self}
+  end
+  
+  
 end
